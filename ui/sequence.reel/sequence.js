c654796e25a748a749c9ec66c9efdc3f78e423ae
hocta <noreply>
2017-01-10 11:40:46 +0000

    Add component sequence

---
+++ ui/sequence.reel/sequence.js
@@ -0,0 +1,16 @@
+/**
+ * @module ui/sequence.reel
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Sequence
+ * @extends Component
+ */
+exports.Sequence = Component.specialize(/** @lends Sequence# */ {
+    constructor: {
+        value: function Sequence() {
+            this.super();
+        }
+    }
+});
